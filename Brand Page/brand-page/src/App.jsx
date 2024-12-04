9425eef92644ec2f2c40af537faeec2f44625e45
naresh <nareshkandpald4@gmail.com>
2024-12-04 13:41:45 +0530

    Commit 2

---
+++ Brand Page/brand-page/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.webp" alt="logo" />
+        </div>
+
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        
+        <button>Login</button>
+      </nav>
+    </>
+  )
+}
+
+export default App
